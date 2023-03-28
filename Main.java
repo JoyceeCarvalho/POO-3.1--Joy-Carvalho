cba0861f88e82853d6f61ffa5dc2545cd28b18f4
Joy Carvalho <94726085+JoyceeCarvalho@users.noreply.github.com>
2023-03-27 21:42:08 -0300

    Funcoes Java- Longevidade.com

---
+++ Main.java
@@ -0,0 +1,19 @@
+package Longevidade.com;
+
+public class PrimeiraFuncao {
+    public static void public static void main(String[] args) {
+        Funcao1();
+        
+    }
+
+    static void Funcao1() {
+        System.out.println("Olá, essa é a minha primeira funcao-Longevidade.com");
+
+    }
+
+    static void Funcao2() {
+        System.out.println(
+                "PROJETO: Longevidade.com Descrição: Com nome longevidade.com, o projeto tem como intenção ajudar idosos e cuidadores a melhor organização de suas rotinas, cuidados, prevenções e apoios. A idéia princial trás o listamento de informações pessoais de saúde e apoio com iniciativas de incluir os idoso nas atividades culturais da cidade, no listamento de números e contatos de emergência, lista de contatos de familiares, planilha com ajendamentos de remédios e consultas, cartilhas de cuidados, higiene, alimentaçao, etc.");
+
+    }
+}
